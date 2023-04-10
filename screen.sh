96c4a30c6a0b50f5bf2f1a7692cd00bacf01532b
nf02 <nicoferru190@gmail.com>
2023-04-10 23:40:47 +0200

    update documentation

--- screen.sh
+++ screen.sh
@@ -2,6 +2,23 @@
 screen1=$(xrandr --listmonitors | awk '{print $2}' | grep - | sed 's/+//' | sed 's/*//' | grep "HDMI-A") 
 screen2=$(xrandr --listmonitors | awk '{print $2}' | grep - | sed 's/+//' | sed 's/*//' | grep "DVI")
 screen3=$(xrandr --listmonitors | awk '{print $2}' | grep - | sed 's/+//' | sed 's/*//' | grep "HDMI-2")
+# screen layout
+#  __________________________                          ________________
+# |                          |                        |                |
+# |                          |                        |                |
+# |                          |                        |                |
+# |         screen1          |                        |                |
+# |                          |                        |                |
+# |                          |                        |     screen2    |
+# |__________________________| _____________________  |                |
+#                             |                     | |                |
+#                             |                     | |                |
+#                             |                     | |                |
+#                             |       screen3       | |                |
+#                             |                     | |________________|
+#                             |                     |
+#                             |_____________________|
+ 
 
 xrandr --output DisplayPort-0 --off --output $screen1 \
        --mode 1920x1080 --pos 0x321 --rotate normal --output $screen2 --mode 1920x1080 \
